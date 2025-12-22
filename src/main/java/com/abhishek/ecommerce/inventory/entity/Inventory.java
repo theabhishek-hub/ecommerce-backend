8646a6452920ea0cee589439dbde143db9646049
Abhishek Kumar <Abhishekkumar15086@gmail.com>
2025-12-22 13:05:44 +0530

    feat: add inventory domain entity

---
+++ src/main/java/com/abhishek/ecommerce/inventory/entity/Inventory.java
@@ -0,0 +1,20 @@
+package com.abhishek.ecommerce.inventory.entity;
+
+import com.abhishek.ecommerce.common.entity.BaseEntity;
+import com.abhishek.ecommerce.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+public class Inventory extends BaseEntity {
+
+    @OneToOne
+    @JoinColumn(name = "product_id", unique = true)
+    private Product product;
+
+    private Integer availableQuantity;
+}
+
