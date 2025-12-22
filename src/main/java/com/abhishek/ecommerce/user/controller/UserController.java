6a193b177ca1c921bfc35ba7cda1f32adb716591
Abhishek Kumar <Abhishekkumar15086@gmail.com>
2025-12-22 15:39:55 +0530

    feat: add user controller

---
+++ src/main/java/com/abhishek/ecommerce/user/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.abhishek.ecommerce.user.controller;
+
+import com.abhishek.ecommerce.user.entity.User;
+import com.abhishek.ecommerce.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * REST controller for User APIs.
+ *
+ * Controllers should:
+ * - Be thin
+ * - Delegate logic to service layer
+ */
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(
+            @PathVariable Long id,
+            @RequestBody User user
+    ) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
+
