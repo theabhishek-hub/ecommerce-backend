fff7f327e882a56216ed234178d59109d41b5751
Abhishek Kumar <Abhishekkumar15086@gmail.com>
2026-01-15 15:57:39 +0530

    Add Thymeleaf UI layout with navbar, footer and CSS

--- src/main/java/com/abhishek/ecommerce/ui/controller/PageController.java
+++ src/main/java/com/abhishek/ecommerce/ui/controller/PageController.java
@@ -1,14 +1,18 @@
 package com.abhishek.ecommerce.ui.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
     @GetMapping("/")
-    public String root() {
-        return "redirect:/products";
+    public String home(Model model) {
+        model.addAttribute("message", "This text is coming from controller");
+        return "index";
     }
 }
+
+
 
